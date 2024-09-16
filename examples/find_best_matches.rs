e5a3fc6d83b5304dbcfed75f3478e91dbf05e84a
preco21 <plusb21@gmail.com>
2024-09-16 16:38:44 +0900

    feat: add find best matches example

---
+++ examples/find_best_matches.rs
@@ -0,0 +1,24 @@
+use anyhow::Result;
+use opencv as cv;
+use opencv_match::prelude::{TryFromCv, TryIntoCv};
+
+fn main() -> Result<()> {
+    let img_tmp = image::open("./examples/up.png")?.to_rgba8();
+    let img_src = image::open("./examples/sample.png")?.to_rgba8();
+
+    let template = opencv_match::Template::new(
+        opencv_match::convert::mat_to_grayscale(&img_tmp.clone().try_into_cv()?, true)?,
+        Default::default(),
+    )?;
+
+    let start = std::time::Instant::now();
+    let res = template.find_best_matches(&opencv_match::convert::mat_to_grayscale(
+        &img_src.try_into_cv()?,
+        true,
+    )?)?;
+    println!("Elapsed: {:?}", start.elapsed());
+
+    println!("Best matches: {:?}", res);
+
+    Ok(())
+}

256dd8c73d59eb1820ccd800d58ad4859881aa9e
preco21 <plusb21@gmail.com>
2026-01-17 16:49:32 +0900

    fix: support for multi matcher

--- src/template/multi.rs
+++ src/template/multi.rs
@@ -2,17 +2,27 @@ use anyhow::Result;
 use opencv::{self as cv};
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
-use super::{FindBestMatchesConfig, MatchResult, Template};
+use super::{FindBestMatchesConfig, MatchResult, Template, TemplatePyramid};
 
 #[derive(Debug, Clone)]
 pub struct MultiMatcherDescriptor {
     pub label: String,
-    pub template: Template,
+    pub matcher: TemplateMatcher,
 }
 
 impl MultiMatcherDescriptor {
     pub fn new(label: String, template: Template) -> Self {
-        Self { label, template }
+        Self {
+            label,
+            matcher: TemplateMatcher::Single(template),
+        }
+    }
+
+    pub fn with_pyramid(label: String, pyramid: TemplatePyramid) -> Self {
+        Self {
+            label,
+            matcher: TemplateMatcher::Pyramid(pyramid),
+        }
     }
 }
 
@@ -45,18 +55,7 @@ impl MultiMatcher {
         let every_matches = self
             .descriptors
             .par_iter()
-            .flat_map(|d| match d.template.find_matching_points(input) {
-                Ok(matches) => matches
-                    .into_iter()
-                    .map(move |m| {
-                        Ok(MultiMatcherResult {
-                            label: d.label.clone(),
-                            result: m,
-                        })
-                    })
-                    .collect::<Vec<_>>(),
-                Err(e) => vec![Err(e)],
-            })
+            .flat_map(|d| d.matcher.find_matching_points(input, &d.label))
             .collect::<Result<Vec<_>, _>>()?;
         let keep = MatchResult::calc_nms_indices(
             &every_matches
@@ -71,7 +70,43 @@ impl MultiMatcher {
 }
 
 #[derive(Debug, Clone)]
+pub enum TemplateMatcher {
+    Single(Template),
+    Pyramid(TemplatePyramid),
+}
+
+impl TemplateMatcher {
+    fn find_matching_points(
+        &self,
+        input: &cv::core::Mat,
+        label: &str,
+    ) -> Result<Vec<MultiMatcherResult>> {
+        match self {
+            TemplateMatcher::Single(template) => Ok(template
+                .find_matching_points(input)?
+                .into_iter()
+                .map(|result| MultiMatcherResult {
+                    label: label.to_string(),
+                    scale: None,
+                    result,
+                })
+                .collect()),
+            TemplateMatcher::Pyramid(pyramid) => Ok(pyramid
+                .find_matching_points(input)?
+                .into_iter()
+                .map(|m| MultiMatcherResult {
+                    label: label.to_string(),
+                    scale: Some(m.scale),
+                    result: m.result,
+                })
+                .collect()),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct MultiMatcherResult {
     pub label: String,
+    pub scale: Option<f64>,
     pub result: MatchResult,
 }
